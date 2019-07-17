db0249ad7fbf5788842454c368e6e7b0ee630f7b
Ryan Punt <ryan@mirum.org>
2019-07-17 12:55:28 -0500

    DRY LED logic

--- arduino_parking_sensor.ino
+++ arduino_parking_sensor.ino
@@ -8,20 +8,29 @@
 //--------
 // DEFINES
 //--------
-
-#define GREEN_LED    7
-#define YELLOW_LED   6
-#define RED_LED      5
+// ultrasonic sensor setup
 #define TRIGGER_PIN  11
 #define ECHO_PIN     12
+
+// LED brightness
+#define LED_DEFAULT_BRIGHTNESS 30
+#define LED_WARNING_BRIGHTNESS 127
+
+// distance measurement limits
 #define MAX_DISTANCE 200
+#define GREEN_MIN    46
+#define YELLOW_MAX   45
+#define YELLOW_MIN   40
+#define RED_MAX      39
+#define RED_MIN      35
 
-#define GREEN_MIN  46
-#define YELLOW_MAX 45
-#define YELLOW_MIN 40
-#define RED_MAX    39
-#define RED_MIN    35
+// set color shortcuts for LED lighting
+#define OFF    0
+#define GREEN  1
+#define YELLOW 2
+#define RED    3
 
+// initialize the LED shield
 #define LED_DRIVER_PIN 13
 
 // Parameter 1 = number of pixels in strip
@@ -51,21 +60,18 @@ void setup() {
 
 void loop() {
   delay(50);
-  
-  pinMode(GREEN_LED, OUTPUT);
-  pinMode(YELLOW_LED, OUTPUT);
-  pinMode(RED_LED, OUTPUT);
+
   distance = (sonar.ping_in());
   Serial.println(distance);
 
   if (distance > GREEN_MIN) {
-    green_led();
+    light_led(GREEN,30);
   }
   else if (distance <= YELLOW_MAX  && distance > YELLOW_MIN) {
-    yellow_led();
+    light_led(YELLOW,50);
   }
   else if (distance <= RED_MAX && distance > RED_MIN) {
-    red_led();
+    light_led(RED,50);
   }
   else if (distance <= RED_MIN) {
     stopp();
@@ -78,37 +84,42 @@ void loop() {
 //----------
 // FUNCTIONS
 //----------
-
-void green_led() {
+void light_led(int color, int brightness {
+  // OFF    0
+  // GREEN  1
+  // YELLOW 2
+  // RED    3
+  int red = 0; int green = 0; int blue = 0;
+  
+  switch (color) { 
+    case 1: 
+      // green
+      green = brightness; 
+      break; 
+    case 2: 
+      // yellow
+      red = brightness;
+      green = brightness;
+      break; 
+    case 3: 
+      // red
+      red = 0;
+      break; 
+    default: 
+      break;   
+  } 
   for(int i=0; i < pixleCount; i++) {
-    pixelShield.setPixelColor(i, pixelShield.Color(0,30,0));
-  }
-  pixelShield.show();
-}
-
-void yellow_led() {
-  for(int i=0; i < pixleCount; i++) {
-    pixelShield.setPixelColor(i, pixelShield.Color(127,127,0));
-  }
-  pixelShield.show();
-}
-
-void red_led() {
-  for(int i=0; i < pixleCount; i++) {
-    pixelShield.setPixelColor(i, pixelShield.Color(127,0,0));
+    pixelShield.setPixelColor(i, pixelShield.Color(red,green,blue));
   }
   pixelShield.show();
 }
 
 void stopp() {
-  red_led();
+  light_led(RED,127);
   delay(50);
   off();
 }
 
 void off() {
-  for(int i=0; i< pixelShield.numPixels(); i++) {
-    pixelShield.setPixelColor(i, pixelShield.Color(0,0,0));
-  }
-  pixelShield.show();
+  light_led(OFF,0);
 }
